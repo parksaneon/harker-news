048eb30b71117f494988396241fb16a0e50a3a32
parksaneon <sangeon1500@daum.net>
2021-06-01 16:58:33 +0900

    feat : router

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
+const ul = document.createElement('ul');
+
 function getData(url) {
     ajax.open('GET', url, false);
     ajax.send();
@@ -11,39 +13,49 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
 
-window.addEventListener('hashchange', function () {
-    const id = this.location.hash.substring(1);
+    newsList.push('<ul>');
 
-    const newsContent = getData(CONTENT_URL.replace('@id', id));
-    const title = document.createElement('h1');
-
-    newsContent.innerHTML = `
-        <h1>${newsContent.title}</h1>
-
-        <div>
-            <a href="#">목록으로</a>
-        </div>
-    `;
-});
-
-const newsList = [];
-
-newsList.push('<ul>');
-
-for (let i = 0; i <ul 10; i++) {
-    const div = document.createElement('div');
-
-    newsList.push(`
+    for (let i = 0; i < 10; i++) {
+        newsList.push(`
         <li>
             <a href="#${newsFeed[i].id}"> 
                 ${newsFeed[i].title} ${newsFeed[i].comments_count}
             </a>
         </li>
     `);
-}
-newsList.push('</ul>');
+    }
+    newsList.push('</ul>');
 
-container.innerHTML = newsList.join('');
+    container.innerHTML = newsList.join('');
+}
+
+function newsDetail() {
+    const id = location.hash.substring(1);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
+
+    container.innerHTML = `
+        <h1>${newsContent.title}</h1>
+
+        <div>
+            <a href="#">목록으로</a>
+        </div>
+    `;
+}
+
+function router() {
+    const routePath = location.hash;
+
+    if (routePath === '') {
+        newsFeed();
+    } else {
+        newsDetail();
+    }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
