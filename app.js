bd140b8e379b4cc721ecc08159f9dbd405bd0a36
parksaneon <sangeon1500@daum.net>
2021-06-01 14:24:47 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const ajax = new XMLHttpRequest();
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+
+ajax.open('GET', NEWS_URL, false);
+ajax.send();
+
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 10; i++) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+
+    a.href = '#';
+    a.innerHTML = `${newsFeed[i].title} ${newsFeed[i].comments_count}`;
+
+    li.appendChild(a);
+    ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);
